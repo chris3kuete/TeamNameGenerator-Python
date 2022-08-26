f7510c0587d86b524f4d6eefd9f2042be884e475
chris3kuete <chriskuete@yahoo.fr>
2022-08-26 14:42:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Team Name Generator.")
+street = input("What is the name of the city where the team will practice ?\n")
+animal_name = input("What is your favorite super hero name?\n")
+print("Your Team name could be " + street + " " + animal_name)
